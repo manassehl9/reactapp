c02e3f8dd370273216362dd13502fc48efe562dc
unknown <manassehl9@gmail.com>
2018-10-01 08:51:10 +0100

    added a functionality when no ingredient is in the burger

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -40,27 +40,37 @@ class BurgerBuilder extends Component {
         this.setState({totalPrice: newwPrice, ingredients: updatedIngredients});
     }
 
-    deleteIngredientHandler = (type) => {
+    removeIngredientHandler = (type) => {
         const oldCount = this.state.ingredients[type];
+        if(oldCount <= 0){
+            // nothing will happen
+            return;
+        }
         const updatedCount = oldCount - 1;
         const updatedIngredients = {
             ...this.state.ingredients
         };
         updatedIngredients[type] = updatedCount;
-        const priceAddtion = INGREDIENT_PRICES[type];
+        const priceDeduction = INGREDIENT_PRICES[type];
         const oldPrice = this.state.totalPrice;
-        const newwPrice = oldPrice - priceAddtion;
+        const newwPrice = oldPrice - priceDeduction;
         this.setState({totalPrice: newwPrice, ingredients: updatedIngredients});
     }
 
     render() {
+        const disabledInfo = {
+            ...this.state.ingredients
+        };
+        for(let key in disabledInfo) {
+            disabledInfo[key] = disabledInfo[key] <= 0
+        }
         return (
             <Auxiliary>
                 <Burger ingredients={this.state.ingredients} />
                 <BuildControls
                     ingredientAdded = {this.addIngredientHandler}
-                    ingredientDeleted = {this.deleteIngredientHandler}
-                     />
+                    ingredientRemoved = {this.removeIngredientHandler}
+                    disabled={disabledInfo} />
             </Auxiliary>
         );
     }
